11c2db89a592623b3e414d897bb98cb340ad3eda
Farcas Rares <81974576+FarcasRares@users.noreply.github.com>
2022-05-25 20:15:47 +0300

    Create ChessBoard.java

---
+++ ChessBoard.java
@@ -0,0 +1,36 @@
+package clean.code.chess.requirements;
+
+public class ChessBoard {
+
+    public static int MAX_BOARD_WIDTH = 7;
+    public static int MAX_BOARD_HEIGHT = 7;
+
+    private Pawn[][] pieces;
+
+    public ChessBoard() {
+        pieces = new Pawn[MAX_BOARD_WIDTH][MAX_BOARD_HEIGHT];
+
+    }
+
+    public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
+        if (IsLegalBoardPosition(xCoordinate,yCoordinate)==true) {
+                pawn.setXCoordinate(xCoordinate);
+                pawn.setYCoordinate(yCoordinate);
+                pawn.setChessBoard(this);
+                Pawn aux=new Pawn(pieceColor); //in case that the sent pawn doesn't have the color setted
+                aux.setXCoordinate(xCoordinate);
+                aux.setYCoordinate(yCoordinate);
+              this.pieces[xCoordinate][yCoordinate] = aux;
+        }
+       // throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+    }
+
+    public boolean IsLegalBoardPosition(int xCoordinate, int yCoordinate) {
+        boolean status=true;
+        if (xCoordinate>MAX_BOARD_WIDTH || xCoordinate<0 || yCoordinate>MAX_BOARD_HEIGHT || yCoordinate <0) {
+            status=false;
+           // throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+        }
+        return status;
+    }
+}
